682f4c2310d732b686a57396303f759b3ef6aff9
Vincent Martyr <vincentmartyr@gmail.com>
2020-02-14 01:21:42 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,53 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(array)
+# counter = 0
+#hash = []
+# while counter < array.length do
+#   puts "#{counter}". array[counter]
+#   counter += 1
+#array.each_with_index { |item, index|
+  #hash[item] = index + 1
+#}
+#puts hash
+array.each.with_index(1) do |value, index|
+  puts "#{index}. #{value}"
+end
+
+end
+
+
+def summon_captain_planet(array)# code an argument here
+  calls = []
+  calls = array.map do |e|
+    "#{e.capitalize }!"
+  end
   # Your code here
 end
 
-def summon_captain_planet# code an argument here
+def long_planeteer_calls(array)# code an argument here
+array.any? {|words| words.length > 4}
   # Your code here
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def find_the_cheese(array)
+cheese_types = ["cheddar", "gouda", "camembert"]
+result = ""
+#  counter = 0
+#  while counter < array
+#array.find do |num|
+#  num.include(cheese_types)
+#    end
+#      counter += 1
+#    end
+if array.detect { |num| num == "cheddar"}
+p result = "cheddar"
+if array.detect { |num| num == "gouda"}
+p result = "gouda"
+if array.detect { |num| num == "camembert"}
+p result = "camembert"
+end
+end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+result
+
 end
